619d414587b6fa4b4bd8c0b9a98aa6d5a2354eb1
Muhammd-Shehzad <127086042+Muhammd-Shehzad@users.noreply.github.com>
2024-06-13 17:32:09 +0500

    Dice App

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void mian() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(child: Center(child: Text('hhhhh'),),),
+      ),
+    ),
+  );
+}
